e1d22025e82e703cf8370fdef48309b02a96556b
Santiago0930 <santiago.guerrerod@javeriana.edu.co>
2025-05-18 20:42:00 -0500

    Correccion test de integracion

--- src/test/java/com/backend/frutti/Java/AuthControllerIntegrationTest.java
+++ src/test/java/com/backend/frutti/Java/AuthControllerIntegrationTest.java
@@ -1,6 +1,7 @@
 package com.backend.frutti.Java;
 
 import com.backend.frutti.DTOs.LoginRequestDTO;
+import com.backend.frutti.DTOs.UsuarioDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.junit.jupiter.api.Test;
@@ -9,11 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@ActiveProfiles("test")
 @SpringBootTest
 @AutoConfigureMockMvc
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -27,9 +30,26 @@ public class AuthControllerIntegrationTest {
 
     @Test
     public void login_Exitoso() throws Exception {
+        UsuarioDTO nuevoUsuario = UsuarioDTO.builder()
+                .email("login@email.com")
+                .password("1234")
+                .nombre("Nombre Inicial")
+                .edad(25)
+                .frutasAnalizadas(0)
+                .genero("Male")
+                .build();
+
+        String response = mockMvc.perform(post("/usuario/registrarUsuario")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(nuevoUsuario)))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+
+        UsuarioDTO usuarioRegistrado = objectMapper.readValue(response, UsuarioDTO.class);
+
         LoginRequestDTO request = new LoginRequestDTO();
-        request.setEmail("flujoAcualizado@email.com");
-        request.setPassword("1234");
+        request.setEmail(usuarioRegistrado.getEmail());
+        request.setPassword(nuevoUsuario.getPassword());
 
         mockMvc.perform(post("/auth/login")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -47,6 +67,6 @@ public class AuthControllerIntegrationTest {
         mockMvc.perform(post("/auth/login")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(request)))
-                .andExpect(status().isUnauthorized());
+                .andExpect(status().is4xxClientError());
     }
 }
